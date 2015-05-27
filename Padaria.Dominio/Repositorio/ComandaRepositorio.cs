8f492fb724a53cecea952928382d68a7ca55b45a
mikemajesty <mikemajesty2011@gmail.com>
2015-05-27 17:33:09 -0300

    Repositorio Comanda

---
+++ Padaria.Dominio/Repositorio/ComandaRepositorio.cs
@@ -0,0 +1,46 @@
+﻿using Padaria.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padaria.Dominio.Repositorio
+{
+    public class ComandaRepositorio
+    {
+        private readonly _DbContext banco = new _DbContext();
+        private const int Sucesso = 1;
+        private const int Insucesso = 0;
+
+        public _DbContext Banco
+        {
+            get { return banco; }
+
+        }
+        public int Cadastrar(Comanda comanda)
+        {
+            Banco.Entry(comanda).State = System.Data.Entity.EntityState.Added;
+            return Banco.SaveChanges() == Sucesso ? Sucesso : Insucesso;
+        }
+
+        public IQueryable<Comanda> Listar()
+        {
+            return this.Banco.Comanda;
+        }
+        public Comanda GetComanda(int ComandaID)
+        {
+            return Banco.Comanda.Find(ComandaID);
+        }
+        public int Deletar(Comanda comanda)
+        {
+            Banco.Entry(comanda).State = System.Data.Entity.EntityState.Deleted;
+            return Banco.SaveChanges() == Sucesso ? Sucesso : Insucesso;
+        }
+        public int Editar(Comanda comanda)
+        {
+            Banco.Entry(comanda).State = System.Data.Entity.EntityState.Modified;
+            return Banco.SaveChanges() == Sucesso ? Sucesso : Insucesso;
+        }
+    }
+}
