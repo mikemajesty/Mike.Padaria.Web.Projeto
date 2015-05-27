7ab982485c8adbf1686d602fe7f1e63e2588fa48
mikemajesty <mikemajesty2011@gmail.com>
2015-05-27 17:35:06 -0300

    Repositorio Fiado

---
+++ Padaria.Dominio/Repositorio/FiadoRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using Padaria.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padaria.Dominio.Repositorio
+{
+    public class FiadoRepositorio
+    {
+        private _DbContext _banco = new _DbContext();
+
+        public _DbContext Banco
+        {
+            get { return _banco; }
+        }
+        public IQueryable<Fiado> Listar()
+        {
+            return Banco.Fiado.Include(c => c.Usuario);
+        }
+
+    }
+}
