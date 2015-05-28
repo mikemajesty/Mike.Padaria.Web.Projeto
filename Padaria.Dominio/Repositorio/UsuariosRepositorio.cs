b62dd13042b947ac76f05bb00680c5335db30964
mikemajesty <mikemajesty2011@gmail.com>
2015-05-28 08:49:42 -0300

    Repositorio Usuario

---
+++ Padaria.Dominio/Repositorio/UsuariosRepositorio.cs
@@ -0,0 +1,49 @@
+﻿using Padaria.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padaria.Dominio.Repositorio
+{
+    public class UsuariosRepositorio
+    {
+        private _DbContext _banco = new _DbContext();
+        private const int Sucesso = 1;
+        private const int Insucesso = 0;
+        public _DbContext Banco
+        {
+            get { return _banco; }
+        }
+
+        public IQueryable<Usuarios> Listar()
+        {
+            return Banco.Usuarios.Include(c => c.Permissao);
+        }
+        public int Salvar(Usuarios usuarios)
+        {
+            Banco.Entry(usuarios).State = EntityState.Added;
+            return Banco.SaveChanges() == Sucesso ? Sucesso : Insucesso;
+
+        }
+
+        public Usuarios GetUsuario(int UsuarioID)
+        {
+            return Banco.Usuarios.Find(UsuarioID);
+        }
+
+        public int Editar(Usuarios usuario)
+        {
+            Banco.Entry(usuario).State = EntityState.Modified;
+            return Banco.SaveChanges() == Sucesso ? Sucesso : Insucesso;
+        }
+
+        public int Deletar(Usuarios usuario)
+        {
+            Banco.Entry(usuario).State = EntityState.Deleted;
+            return Banco.SaveChanges() == Sucesso ? Sucesso : Insucesso;
+        }
+    }
+}
