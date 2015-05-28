6ad3b536eab259ec1246b20e1ed7b572dfb98e9b
mikemajesty <mikemajesty2011@gmail.com>
2015-05-28 11:33:32 -0300

    Tipo de Pagamento Controller Usuario Controller

---
+++ Padaria.View/Controllers/TipoDePagamentoController.cs
@@ -0,0 +1,73 @@
+﻿using Padaria.Dominio.Entidades;
+using Padaria.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Padaria.View.Controllers
+{
+    public class TipoDePagamentoController : Controller
+    {
+        private TipoDePagamentoRepositorio tipoPagamentoDB = null;
+        private const int Sucesso = 1;
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            tipoPagamentoDB = new TipoDePagamentoRepositorio();
+            return View(tipoPagamentoDB.Listar());
+        }
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View(new TipoPagamento());
+        }
+        [HttpPost]
+        public ActionResult Cadastrar(TipoPagamento tipoPagamento)
+        {
+            tipoPagamentoDB = new TipoDePagamentoRepositorio();
+
+            if (tipoPagamentoDB.Cadastrar(tipoPagamento) == Sucesso)
+            {
+                return RedirectToAction("Listar");
+            }
+            return View(tipoPagamento);
+        }
+        [HttpGet]
+        public ActionResult Editar(int tipoPagamentoID)
+        {
+            tipoPagamentoDB = new TipoDePagamentoRepositorio();
+            TipoPagamento tipoPagamento = tipoPagamentoDB.GetTipoPagamento(tipoPagamentoID);
+            return View(tipoPagamento);
+        }
+        [HttpPost]
+        public ActionResult Editar(TipoPagamento tipoPagamento)
+        {
+            tipoPagamentoDB = new TipoDePagamentoRepositorio();
+            if (tipoPagamentoDB.Editar(tipoPagamento) == Sucesso)
+            {
+                return RedirectToAction("Listar");
+            }
+            return View(tipoPagamento);
+        }
+        [HttpGet]
+        public ActionResult Deletar(int tipoPagamentoID)
+        {
+            tipoPagamentoDB = new TipoDePagamentoRepositorio();
+            TipoPagamento tipoPagamento = tipoPagamentoDB.GetTipoPagamento(tipoPagamentoID);
+            return View(tipoPagamento);
+        }
+        [HttpPost]
+        public ActionResult Deletar(TipoPagamento tipoPagamento)
+        {
+            tipoPagamentoDB = new TipoDePagamentoRepositorio();
+            if (tipoPagamentoDB.Deletar(tipoPagamento) == Sucesso)
+            {
+                return RedirectToAction("Listar");
+            }
+            return View(tipoPagamento);
+        }
+
+    }
+}
---
+++ Padaria.View/Controllers/UsuariosController.cs
@@ -0,0 +1,101 @@
+﻿using Padaria.Dominio.Entidades;
+using Padaria.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Padaria.View.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private UsuariosRepositorio usuariosDB;
+        private const int Sucesso = 1;
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            usuariosDB = new UsuariosRepositorio();
+            return View(usuariosDB.Listar());
+        }
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            usuariosDB = new UsuariosRepositorio();
+            return View(CarregaParaCadastro());
+        }
+
+
+        [HttpPost]
+        public ActionResult Cadastrar(CadastraUsuariosPermissaoViewModel viewModel)
+        {
+            usuariosDB = new UsuariosRepositorio();
+            return usuariosDB.Salvar(viewModel.Usuarios) == Sucesso ? View("Listar", usuariosDB.Listar()) : View(new CadastraUsuariosPermissaoViewModel()
+            {
+                Usuarios = viewModel.Usuarios,
+                Permissao = new SelectList(usuariosDB.Banco.Permissao, "PermissaoID", "Nome")
+
+            });
+        }
+        [HttpGet]
+        public ActionResult Editar(int UsuarioID)
+        {
+            usuariosDB = new UsuariosRepositorio();
+            Usuarios usuario = usuariosDB.GetUsuario(UsuarioID);
+            return View(new CadastraUsuariosPermissaoViewModel()
+            {
+                Usuarios = usuario,
+                Permissao = new SelectList(usuariosDB.Banco.Permissao, "PermissaoID", "Nome", usuario.PermissaoID)
+            });
+        }
+        [HttpPost]
+        public ActionResult Editar(CadastraUsuariosPermissaoViewModel viewModel)
+        {
+            usuariosDB = new UsuariosRepositorio();
+            return (usuariosDB.Editar(viewModel.Usuarios) == Sucesso) ? View("Listar", usuariosDB.Listar()) : View(viewModel);
+
+        }
+        [HttpGet]
+        public ActionResult Detales(int UsuarioID)
+        {
+            usuariosDB = new UsuariosRepositorio();
+            Usuarios usuario = usuariosDB.GetUsuario(UsuarioID: UsuarioID);
+            return View(new CadastraUsuariosPermissaoViewModel()
+            {
+                Usuarios = usuario,
+                Permissao = new SelectList(usuariosDB.Banco.Permissao, "PermissaoID", "Nome", usuario.PermissaoID)
+            });
+        }
+        [HttpGet]
+        public ActionResult Deletar(int UsuarioID)
+        {
+            usuariosDB = new UsuariosRepositorio();
+            Usuarios usuario = usuariosDB.GetUsuario(UsuarioID: UsuarioID);
+            return View(new CadastraUsuariosPermissaoViewModel()
+            {
+                Usuarios = usuario,
+                Permissao = new SelectList(usuariosDB.Banco.Permissao, "PermissaoID", "Nome", usuario.PermissaoID)
+            });
+        }
+        [HttpPost]
+        public ActionResult Deletar(CadastraUsuariosPermissaoViewModel viewModel)
+        {
+            usuariosDB = new UsuariosRepositorio();
+            return usuariosDB.Deletar(viewModel.Usuarios) == Sucesso ? View("Listar", usuariosDB.Listar()) : View(new CadastraUsuariosPermissaoViewModel()
+            {
+                Usuarios = viewModel.Usuarios,
+                Permissao = new SelectList(usuariosDB.Banco.Permissao, "PermissaoID", "Nome")
+
+            });
+        }
+        private CadastraUsuariosPermissaoViewModel CarregaParaCadastro()
+        {
+            return new CadastraUsuariosPermissaoViewModel()
+            {
+                Permissao = new SelectList(usuariosDB.Banco.Permissao, "PermissaoID", "Nome"),
+                Usuarios = new Usuarios() { UltimoAcesso = DateTime.Now.ToString("dd/MM/yyyy H:mm:ss") }
+
+            };
+        }
+    }
+}
