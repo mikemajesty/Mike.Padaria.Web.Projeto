abb06efca8b1a1bf257374f37d351f4b455de2b8
mikemajesty <mikemajesty2011@gmail.com>
2015-05-28 11:35:45 -0300

    Caixa - Propriedades e Data Annotation

--- Padaria.Dominio/Entidades/Caixa.cs
+++ Padaria.Dominio/Entidades/Caixa.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,16 @@ namespace Padaria.Dominio.Entidades
 {
     public class Caixa
     {
+        [Range(minimum: 0, maximum: double.MaxValue, ErrorMessage = "Campo {0} contem valores invalidos")]
+        [DisplayName(displayName: "Valor:")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:C}")]
+        [RegularExpression(@"\d+(\,\d{1.2})?", ErrorMessage = "Campo {0} contém valores Inválido.")]*/
+
+        public decimal Valor { get; set; }
+        [RegularExpression(@"^[0-9]{4}$", ErrorMessage = "Campo {0} só permite numeros.")]       
+        [DisplayName(displayName: "UsuarioID:")]
+        [Key]
+        [Range(minimum: 0, maximum: int.MinValue, ErrorMessage = "Campo {0} só permite valores acima de 1.")]*/
+        public int UsuarioID { get; set; }
     }
 }
