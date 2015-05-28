c1753b473d3510657ce67eb56be8be7c613f9b80
mikemajesty <mikemajesty2011@gmail.com>
2015-05-28 11:53:10 -0300

    Tipo Pagamento - Propriedades e Data Annotations

--- Padaria.Dominio/Entidades/TipoPagamento.cs
+++ Padaria.Dominio/Entidades/TipoPagamento.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Padaria.Dominio.Entidades
 {
     public class TipoPagamento
     {
+        [HiddenInput(DisplayValue = false)]
+        [Range(minimum: 0, maximum: int.MaxValue, ErrorMessage = "Campo {0} só permite valores acima de 1 e 2.147.483.647.")]
+        [RegularExpression(@"^[0-9]{4}$", ErrorMessage = "Campo {0} só permite numeros.")]
+        [Required(ErrorMessage = "Campo {0} é obrigatorio.")]
+        [DisplayName(displayName: "TipoPagamentoID:")]
+        [Key]
+        public int TipoPagamentoID { get; set; }
+        [Required(ErrorMessage = "Campo {0} é obrigatorio.")]
+        [DisplayName(displayName: "Tipo:")]
+        [StringLength(maximumLength: 15, MinimumLength = 3, ErrorMessage = "Campo {0} só permite de 3 a 15 caracteres.")]
+        public string Tipo { get; set; }
     }
 }
